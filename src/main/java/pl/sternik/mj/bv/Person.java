1dab19e18545ac32a215a4da1b100d1c27f02d54
Flantek <mateusz.jacykowski@edu.uekat.pl>
2018-04-07 11:22:05 +0200

    Bean Validation

---
+++ src/main/java/pl/sternik/mj/bv/Person.java
@@ -0,0 +1,32 @@
+package pl.sternik.mj.bv;
+
+import pl.sternik.mj.bv.grupy.Address;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+public class Person {
+
+    @NotNull
+    String name;
+
+    @NotNull
+    @Valid
+    Address address;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
